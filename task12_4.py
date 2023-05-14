97db327cfffd1ec70e31c217b31d4c3eb74000d4
Katya Kulyatina <124413851+kulyatinakatya@users.noreply.github.com>
2023-05-14 22:09:45 +0700

    Update task12_4.py

--- task12_4.py
+++ task12_4.py
@@ -17,13 +17,22 @@ class Library:
         print('author:', self.author, '\n', 'title:', self.title)
         self.__get_full_info(input('Enter the password for full access: '))
 
+    def set_NumOfCop(self):
+        self.__num_of_copies -= 1
+
     def get_book(self, holder):
         if self.__num_of_copies >= 1:
-            self.__num_of_copies -= 1
+            self.set_NumOfCop()
             self.__holder.append(holder)
+
+    def set_NewHolder(self, name1, name2):
+        if (name1 != name2) and (name2 not in self.__holder):
+            self.__holder[self.__holder.index(name1)] = name2
 
 
 book1 = Library('William Shakespeare', 'Romeo and Juliet', 3)
 book1.get_book('Sam White')
 book1.get_book('Eva Green')
+book1.get_info()
+book1.set_NewHolder('Eva Green', 'George')
 book1.get_info()
