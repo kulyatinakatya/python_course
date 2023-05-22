53d6915328c3b4d08ceb430289b70f58abb5434c
Katya Kulyatina <124413851+kulyatinakatya@users.noreply.github.com>
2023-05-22 14:53:02 +0700

    Update task7_3.py

--- task7_3.py
+++ task7_3.py
@@ -7,9 +7,8 @@ text = "This is a file for task 7_3"
 
 def func(path):
     os.makedirs(path, exist_ok=True)
-    new_file = open("TestFile.txt", "w")
-    new_file.write(text)
-    new_file.close()
+    with open('TestFile.txt', 'w') as f:
+        f.write(text)
 
 
 class Test(unittest.TestCase):
@@ -18,10 +17,9 @@ class Test(unittest.TestCase):
         func('/Users/Katya/PycharmProjects/python_course/new_folder')
 
     def testmethod(self):
-        new_file = open("TestFile.txt", "r")
-        self.assertFalse(os.stat('TestFile.txt').st_size == 0)
-        self.assertTrue(text == new_file.read())
-        new_file.close()
+        with open('TestFile.txt', 'r') as f:
+            self.assertFalse(os.stat('TestFile.txt').st_size == 0)
+            self.assertTrue(text == f.read())
 
     def tearDown(self):
         shutil.rmtree('/Users/Katya/PycharmProjects/python_course/new_folder')
