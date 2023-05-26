9f8ad52db36d1f1e493d5f136667ac1d0360690a
kulyatinakatya <e.kulyatina@g.nsu.ru>
2023-05-27 05:36:48 +0700

    Task 2 lesson 3 resolved

--- task3_2.py
+++ task3_2.py
@@ -1,25 +1,22 @@
 import json
 
-with open('jul1.json', 'w') as outfile:
-
-        with open('RomeoAndJuliet.json', 'r') as f:
-            data = json.load(f)
-        dic = {}
+with open('task3_2.json', 'w') as fout:
+    with open('RomeoAndJuliet.json', 'r') as f:
+        data = json.load(f)
         characters = []
-        acts = data['acts']
+        dic = {}
 
-        for line in acts:
-            scene = line['scenes']
-            for line in scene:
-                title = line['title']
-
-                for line in scene:
-                    action = line['action']
-                    for line in action:
-                        character = line['character']
-                        if character not in characters:
-                            characters.append(character)
-                dic[title] = characters
-
-        json_object = json.dumps(dic, indent=4)
-        outfile.write(json_object)
+        for acts in data['acts']:
+            for scenes in acts['scenes']:
+                title = scenes['title']
+                for action in scenes['action']:
+                    for character in action:
+                        if action['character'] not in characters:
+                            characters.append(action['character'])
+                key = title
+                dic[key] = characters
+                json_object = json.dumps(dic)
+                fout.write(json_object)
+                fout.write("\n")
+                characters = []
+                dic = {}
